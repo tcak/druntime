e06d9ba68903d1c28a697dbaa9b2f35c60fa4b28
tcak <tcak@users.noreply.github.com>
2016-04-05 15:58:52 +0300

    init function doesn't check whether malloc fails.
    Not checking the result of malloc causes SEGMENTATION ERROR. This happens if no memory is left while creating a new thread (start method).

--- src/rt/tlsgc.d
+++ src/rt/tlsgc.d
@@ -32,6 +32,7 @@ struct Data
 void* init()
 {
     auto data = cast(Data*).malloc(Data.sizeof);
+    if( data is null ) throw new core.exception.OutOfMemoryError();
     *data = Data.init;
 
     // do module specific initialization
